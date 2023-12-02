ea24014e18c113741197eb17569eadc40a187ed0
nileshshrs <nileshshr54@gmail.com>
2023-12-02 22:49:04 +0545

    innitializing and setting up project with necessary dependancy for project env

---
+++ Frontend/e-bookstore/src/App.jsx
@@ -0,0 +1,13 @@
+import {Routes, Route} from "react-router-dom"
+
+function App() {
+
+
+  return (
+    <>
+      hello world !
+    </>
+  )
+}
+
+export default App
---
+++ e-bookstore/src/main/java/com/example/ebookstore/EBookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.ebookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBookstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EBookstoreApplication.class, args);
+    }
+
+}
