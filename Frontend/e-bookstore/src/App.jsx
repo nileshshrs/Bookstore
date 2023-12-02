9f40a55d56ac734065c9b161103040643c4ef487
nileshshrs <nileshshr54@gmail.com>
2023-12-02 22:57:20 +0545

    enabling routing to the frontend for the project

--- Frontend/e-bookstore/src/App.jsx
+++ Frontend/e-bookstore/src/App.jsx
@@ -1,11 +1,16 @@
 import {Routes, Route} from "react-router-dom"
+import Home from "./components/Home"
 
 function App() {
-
+  // use <Route path="/" element={<Element/>}/> and create element for routes in components folder and replace element 
+  //in route for new routes Default is Home 
 
   return (
     <>
-      hello world !
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+      
     </>
   )
 }
---
+++ Frontend/e-bookstore/src/Firebase/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDzpdTaMr4ZGjYlJxQF8oXpqyPxt5S7Ncs",
+  authDomain: "image-uploads-fc90d.firebaseapp.com",
+  projectId: "image-uploads-fc90d",
+  storageBucket: "image-uploads-fc90d.appspot.com",
+  messagingSenderId: "993530164265",
+  appId: "1:993530164265:web:79013021c389cf1fbc9838"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
