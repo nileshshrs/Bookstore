f6d9c10fddae2f8a89b211e2bed1d94c3c470032
nileshshrs <nileshshr54@gmail.com>
2023-12-08 08:11:12 +0545

    Footer section completed

--- Frontend/e-bookstore/src/App.jsx
+++ Frontend/e-bookstore/src/App.jsx
@@ -1,13 +1,11 @@
-import {Routes, Route} from "react-router-dom"
-import Home from "./components/Home"
-import Register from "./Pages/Register"
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.scss';
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Navigation from "./components/Navigation";
+import Register from "./Pages/Register";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.scss";
 import Login from "./Pages/Login";
-
-
-
-
+import Footer from "./components/Footer";
 
 function App() {
   // use <Route path="/" element={<Element/>}/> and create element for routes in components folder and replace element
@@ -15,14 +13,14 @@ function App() {
 
   return (
     <>
+      <Navigation />
       <Routes>
-
-        <Route path="/" element={<Home/>}/>
-        <Route path="/register" element={<Register/>}/>
-        <Route path="/login" element={<Login/>}/>
-
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
 
+      <Footer />
     </>
   );
 }
