324355237ebcca3a33ec84cc374e660cc4b97b40
nileshshrs <nileshshr54@gmail.com>
2023-12-09 16:09:57 +0545

    responsive navbar

--- Frontend/e-bookstore/src/components/Navigation.jsx
+++ Frontend/e-bookstore/src/components/Navigation.jsx
@@ -1,45 +1,145 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import "../css/navigation.scss";
+import { FaSearch, FaShoppingBag } from "react-icons/fa";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Navigation = () => {
+  const [isHeaderFixed, setIsHeaderFixed] = useState(false);
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      setIsHeaderFixed(scrollPosition > 60);
+    };
+
+    // Attach the event listener when the component mounts
+    window.addEventListener("scroll", handleScroll);
+
+    // Detach the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
-    <header>
-      <div className="px-11 py-3 flex justify-evenly items-center w-full">
-        <div>
+    <header className={`bg-[#EDEBE4] ${isHeaderFixed ? "fixed-header" : ""}`}>
+      <div className="px-2 py-3 flex justify-evenly items-center w-full gap-3 nav-bar">
+        <div className="px-2">
           <Link to="/">
             <h2 className="text-[22px]">Bookstore</h2>
           </Link>
         </div>
-        <nav className="w-full px-3 flex">
+        <nav className="w-full px-5 flex">
           <ul className="flex w-full items-center justify-evenly">
-            <li>
-              <Link to="">Home</Link>
+            <li className="">
+              <Link to="/" className="w-full font-bold">
+                Home
+              </Link>
             </li>
-            <li>
-              <Link to="">Books</Link>
+            <li className="">
+              <Link to="" className="w-full font-bold">
+                Books
+              </Link>
             </li>
-            <li>
-              <Link to="">Blog</Link>
+            <li className="">
+              <Link to="" className="w-full font-bold">
+                Blog
+              </Link>
             </li>
-            <li>
+            <li className="">
+              <Link to="" className="w-full font-bold">
+                Contact
+              </Link>
+            </li>
+            <li className="">
               <form className="search-form flex">
-                <input type="text" />
-                <button>search</button>
+                <input type="text" placeholder="search..." />
+                <button>
+                  <FaSearch />
+                </button>
               </form>
             </li>
           </ul>
-          <div className="flex justify-between items-center gap-3">
+          <div className="flex justify-between items-center gap-3 btn-div">
             <div>
-              <button>Sign up/Login</button>
+              <Link to="/login">
+                <button className="login-btn">Login</button>
+              </Link>
             </div>
             <div>
-              <button>Account</button>
+              <Link to="">
+                <button className="account-btn">Account</button>
+              </Link>
             </div>
             <div>
-              <button>Cart</button>
+              <Link to="">
+                <button className="cart">
+                  <FaShoppingBag />
+                </button>
+              </Link>
             </div>
           </div>
         </nav>
+        <div
+          className={
+            nav
+              ? "sidebar-nav slide flex flex-col gap-5 relative"
+              : "sidebar-nav flex flex-col gap-5 relative"
+          }
+        >
+          <button className="absolute top-10 right-0" onClick={handleNav}>
+            <AiOutlineClose />
+          </button>
+          <div className="w-full my-5 text-[22px] px-3">
+            <Link>
+              <h2>Bookstore</h2>
+            </Link>
+          </div>
+          <nav className="h-full flex justify-center items-center">
+            <ul className="h-full">
+              <li>
+                <Link className="w-full font-bold" to="">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link className="w-full font-bold" to="">
+                  Books
+                </Link>
+              </li>
+              <li>
+                <Link className="w-full font-bold" to="">
+                  Blog
+                </Link>
+              </li>
+              <li>
+                <Link className="w-full font-bold" to="">
+                  Contact
+                </Link>
+              </li>
+              <li>
+                <Link className="flex items-center justify-start w-full font-bold gap-2">
+                  <span> Cart </span>
+                  <FaShoppingBag />
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          <div className=" flex flex-col items-center jusify-center gap-2 my-10 w-full px-3">
+            <button className="account-btn">Account</button>
+            <button className="login-btn">Login</button>
+          </div>
+        </div>
+        <div className="hamburger-menu">
+          <button className="hamburger-btn" onClick={handleNav}>
+            <AiOutlineMenu />
+          </button>
+        </div>
       </div>
     </header>
   );
--- Frontend/e-bookstore/src/main.jsx
+++ Frontend/e-bookstore/src/main.jsx
@@ -4,16 +4,15 @@ import App from "./App.jsx";
 import "./index.scss";
 import { BrowserRouter } from "react-router-dom";
 import { AuthContextProvider } from "./context/AuthContext.jsx";
-import {AuthContextProvider} from './AuthContext'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthContextProvider>
     <BrowserRouter>
       <AuthContextProvider>
-        <App />
+        <AuthContextProvider>
+          <App />
+        </AuthContextProvider>
       </AuthContextProvider>
     </BrowserRouter>
-    </AuthContextProvider>
   </React.StrictMode>
 );
