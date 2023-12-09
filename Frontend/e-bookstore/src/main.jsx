313c44b48da041b4260cd2b559113a39757bfff5
Sujala99 <sujala.rai2004@gmail.com>
2023-12-09 11:52:20 +0545

    login connection with backend

--- Frontend/e-bookstore/src/main.jsx
+++ Frontend/e-bookstore/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.scss";
 import { BrowserRouter } from "react-router-dom";
+import {AuthContextProvider} from './AuthContext'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <AuthContextProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </AuthContextProvider>
   </React.StrictMode>
 );
