80cb28e4268e5976a9284b52fffa2325a214ce52
nileshshrs <nileshshr54@gmail.com>
2023-12-13 23:56:00 +0545

    form design for creating books as well as api connection finished

--- Frontend/e-bookstore/src/components/DashboardProducts.jsx
+++ Frontend/e-bookstore/src/components/DashboardProducts.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { useState } from "react";
+import { FaPlusCircle } from "react-icons/fa";
+import AddBookForm from "./AddBookForm";
 
 const DashboardProducts = () => {
   return (
-    <div>DashboardProducts</div>
-  )
-}
+    <div className="relative">
+      <button className="absolute right-0 top-[50%] rounded-full border p-3 text-xl">
+        <FaPlusCircle />
+      </button>
+      <AddBookForm />
+    </div>
+  );
+};
 
-export default DashboardProducts
+export default DashboardProducts;
