e1f6ba93c830c78dd91e2d78db46d65881604b56
bibhakta21 <bibhaktalamsal8@gmail.com>
2023-12-20 20:18:16 +0545

    Refactored code

---
+++ Frontend/e-bookstore/src/Pages/Singlepagecart.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import ImgSecrets from "../assets/product-item6.jpg";
+
+const Singlepagecart = () => {
+ 
+
+  return (
+    <>
+     <h2>If design not accepted</h2>
+    </>
+  );
+};
+
+export default Singlepagecart;
