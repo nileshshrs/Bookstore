86b01663a254ecb147d8975ae9c7263f9be2558e
Rohan manandhaar <118821550+Ziroworld@users.noreply.github.com>
2023-12-21 19:52:34 +0545

    Added the main logic of test for add to cart
     the main logic of test for add to cart worked perfectly by checking the HTTP request of 201 in postman( will only pass if the postman sends data to database)

--- e-bookstore/src/test/java/com/example/ebookstore/Controller/AddToCartFuncTest.java
+++ e-bookstore/src/test/java/com/example/ebookstore/Controller/AddToCartFuncTest.java
@@ -44,6 +44,33 @@ public class AddToCartFuncTest {
         MockitoAnnotations.openMocks(this);
         mockMvc = MockMvcBuilders.standaloneSetup(cartController).build();
 
+        // Sample request body for adding to cart
+        HashMap<String, Object> requestBody = new HashMap<>();
+        requestBody.put("userId", 1L);
+        requestBody.put("bookId", 2L);
+        requestBody.put("quantity", 1);
+
+        // Mock service responses
+        Users mockUser = new Users();
+        when(userService.getUsersById(1L)).thenReturn(Optional.of(mockUser));
+
+        Book mockBook = new Book();
+        when(bookService.getBookById(2L)).thenReturn(Optional.of(mockBook));
+
+        Cart mockCartItem = new Cart();
+        when(cartService.addToCart(mockUser, mockBook, 1)).thenReturn(mockCartItem);
+
+        // Perform POST request to /api/v2/carts/add-to-cart endpoint
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/api/v2/carts/add-to-cart")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"userId\":1,\"bookId\":2,\"quantity\":1}")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andReturn();
+
+        // Print the response content for debugging
+        System.out.println("Response Content: " + result.getResponse().getContentAsString());
+
     }
 
 }
