5b20a989023f64cddc777a72702c3e68813f29db
Rohan manandhaar <118821550+Ziroworld@users.noreply.github.com>
2023-12-21 23:30:48 +0545

    initial setup for testing for further part

--- e-bookstore/src/test/java/com/example/ebookstore/Controller/CartControllerTest.java
+++ e-bookstore/src/test/java/com/example/ebookstore/Controller/CartControllerTest.java
@@ -1,5 +1,31 @@
 package com.example.ebookstore.Controller;
 
+import com.example.ebookstore.Service.CartService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 public class CartControllerTest {
+
+    @Mock
+    private CartService cartService;
+
+    @InjectMocks
+    private CartController cartController;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(cartController).build();
+    }
 
 }
