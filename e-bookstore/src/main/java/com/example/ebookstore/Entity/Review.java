13cd31bd7141a7e3934ab4003f8a9af229949d02
ujaann <ujanst@gmail.com>
2023-12-25 14:13:42 +0545

    Added repository and entity of review.

---
+++ e-bookstore/src/main/java/com/example/ebookstore/Entity/Review.java
@@ -0,0 +1,54 @@
+package com.example.ebookstore.Entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "reviews",uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"user_id","book_id"})
+})
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "review_id")
+    private long reviewId;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private Users user;
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
+    @Column(name = "review_text",columnDefinition = "TEXT")
+    private String reviewText;
+
+    public long getReviewId() {
+        return reviewId;
+    }
+
+    public void setReviewId(long reviewId) {
+        this.reviewId = reviewId;
+    }
+
+    public Users getUser() {
+        return user;
+    }
+
+    public void setUser(Users user) {
+        this.user = user;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public String getReviewText() {
+        return reviewText;
+    }
+
+    public void setReviewText(String reviewText) {
+        this.reviewText = reviewText;
+    }
+}
