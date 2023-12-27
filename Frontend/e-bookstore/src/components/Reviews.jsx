d36ef28a5c1a20a57f1c11248b4cfb1f924e31bb
nileshshrs <nileshshr54@gmail.com>
2023-12-27 22:38:50 +0545

    responsive review design

--- Frontend/e-bookstore/src/components/Reviews.jsx
+++ Frontend/e-bookstore/src/components/Reviews.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { useAuthContext } from "../context/useAuthContext";
 import { Link } from "react-router-dom";
+import "../css/reviews.scss";
 
 const Reviews = ({ reviews, bookID, fetchReviews }) => {
   const { user } = useAuthContext();
 
-
   return (
     <div className="review-section w-[80%] mx-auto p-5 flex flex-col gap-4 ">
-      <div className="flex flex-col w-full px-5 gap-3">
+      <div className="flex flex-col w-full px-5 gap-3 review-content">
         <h4 className="font-bold">Write a Review</h4>
         <form
           action=""
@@ -23,19 +23,19 @@ const Reviews = ({ reviews, bookID, fetchReviews }) => {
           </button>
         </form>
       </div>
-      <div className="px-5 flex flex-col gap-3">
+      <div className="px-5 flex flex-col gap-3 reviews">
         <h4 className="font-bold text-md">Reviews: </h4>
         {reviews.map((review) => (
           <div key={review.id} className="px-3">
             <div
-              className="flex flex-col gap-2 px-2 py-1 m-0"
+              className="flex flex-col gap-2 px-2 py-1 m-0 "
               style={{ borderBottom: "1px solid #FFFFFF" }}
             >
               <Link to="">
                 <h4 className="font-bold text-sm">{review.userName}</h4>
               </Link>
               <p className="m-0 text-sm">{review.reviewText}</p>
-              <div className="flex gap-3 py-2">
+              <div className="flex gap-3 py-2 ">
                 {user &&
                   (user.role === "admin" || user.id === review.userId) && (
                     <>
