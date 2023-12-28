995c93cc6b7ac31f98ea2efe0ed87b82bdf1840f
Rohan manandhaar <118821550+Ziroworld@users.noreply.github.com>
2023-12-28 22:28:26 +0545

    Enhanced the block of code.

--- e-bookstore/src/test/java/com/example/ebookstore/Testing/DeleteReviewFuncTest.java
+++ e-bookstore/src/test/java/com/example/ebookstore/Testing/DeleteReviewFuncTest.java
@@ -7,14 +7,9 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import java.util.HashMap;
-import java.util.Map;
-
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
--- e-bookstore/src/test/java/com/example/ebookstore/Testing/GetAllReviewsOfBookFuncTest.java
+++ e-bookstore/src/test/java/com/example/ebookstore/Testing/GetAllReviewsOfBookFuncTest.java
@@ -9,14 +9,11 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.Collections;
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
