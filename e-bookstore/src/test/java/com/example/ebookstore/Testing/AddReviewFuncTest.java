9e149ac8031662f6819c0bbb438800b2ee2e2a12
Rohan manandhaar <118821550+Ziroworld@users.noreply.github.com>
2023-12-28 16:34:07 +0545

    feat: Initialize 'AddReviewFuncTest' with initial setup of methods, class, and attributes
    This commit introduces the foundational code for the 'AddReviewFuncTest' file, setting up the necessary methods, class structure, and attributes to facilitate testing functionalities related to reviews.

--- e-bookstore/src/test/java/com/example/ebookstore/Testing/AddReviewFuncTest.java
+++ e-bookstore/src/test/java/com/example/ebookstore/Testing/AddReviewFuncTest.java
@@ -1,4 +1,53 @@
 package com.example.ebookstore.Testing;
 
+
+import com.example.ebookstore.Entity.Book;
+import com.example.ebookstore.Entity.Users;
+import com.example.ebookstore.Entity.Review;
+import com.example.ebookstore.Service.BookService;
+import com.example.ebookstore.Service.ReviewService;
+import com.example.ebookstore.Service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 public class AddReviewFuncTest {
+
+    @Mock
+    private UserService userService;
+
+    @Mock
+    private BookService bookService;
+
+    @Mock
+    private ReviewService reviewService;
+
+    @InjectMocks
+    private ReviewController reviewController;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(reviewController).build();
+    }
+
+    @Test
+    void testAddReview() throws Exception {
+        
+    }
 }
