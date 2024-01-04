985db63e16b0811f72c5418d98679c194bba876a
Rohan manandhaar <118821550+Ziroworld@users.noreply.github.com>
2024-01-04 19:53:09 +0545

    Feat: Initialized the files for the blog test where it includes test for AddBlogFunc, DeleteBlogFunc, GetBlogByIDFuc

---
+++ e-bookstore/src/test/java/com/example/ebookstore/Testing/AddBlogFuncTest.java
@@ -0,0 +1,24 @@
+package com.example.ebookstore.Testing;
+
+import com.example.ebookstore.Controller.BlogController;
+import com.example.ebookstore.Entity.Blog;
+import com.example.ebookstore.Entity.Users;
+import com.example.ebookstore.Service.BlogService;
+import com.example.ebookstore.Service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.*;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class AddBlogFuncTest {
+
+}
---
+++ e-bookstore/src/test/java/com/example/ebookstore/Testing/GetBlogByIdFuncTest.java
@@ -0,0 +1,25 @@
+package com.example.ebookstore.Testing;
+
+import com.example.ebookstore.Controller.BlogController;
+import com.example.ebookstore.Entity.Blog;
+import com.example.ebookstore.Entity.Users;
+import com.example.ebookstore.Service.BlogService;
+import com.example.ebookstore.Service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class GetBlogByIdFuncTest {
+
+}
