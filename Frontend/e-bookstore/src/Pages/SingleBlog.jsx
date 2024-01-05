b3e1e579c634df9d981b6a93c29863eb2ae14e1b
nileshshrs <nileshshr54@gmail.com>
2024-01-05 14:39:38 +0545

    made comment section responsive, fixed review not being responsive, changed ui design for both to be a but more friendlier

--- Frontend/e-bookstore/src/Pages/SingleBlog.jsx
+++ Frontend/e-bookstore/src/Pages/SingleBlog.jsx
@@ -3,6 +3,7 @@ import "../css/singleblog.scss";
 import img1 from "../assets/post-img2.jpg";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import Comments from "../components/Comments";
 
 const SingleBlog = () => {
   const { id } = useParams();
@@ -14,7 +15,7 @@ const SingleBlog = () => {
         `http://localhost:8080/api/v2/blogs/getById/${id}`
       );
       console.log(res.data);
-      setPost(res.data)
+      setPost(res.data);
     };
     fetchPost();
   }, []);
@@ -24,18 +25,20 @@ const SingleBlog = () => {
       <div className="blog-title-container">
         <div className="blog-title">
           <span>1st Jan, 2024</span>
-          <h2>
-            {post.blogTitle}
-          </h2>
+          <h2>{post.blogTitle}</h2>
           <p>Lorem, ipsum dolor.</p>
         </div>
         <div className="blog-img-container">
-          <img src={post.imagePath} alt="" srcset=""  className="object-contain max-h-[328px]"/>
+          <img
+            src={post.imagePath}
+            alt=""
+            srcset=""
+            className="object-contain max-h-[328px]"
+          />
         </div>
       </div>
-      <article className="posts">
-        {post.blogDetails}
-      </article>
+      <article className="posts">{post.blogDetails}</article>
+      <Comments />
     </section>
   );
 };
--- Frontend/e-bookstore/src/components/Comments.jsx
+++ Frontend/e-bookstore/src/components/Comments.jsx
@@ -1,56 +1,41 @@
 import React, { useState } from "react";
 import { useAuthContext } from "../context/useAuthContext";
 import { Link } from "react-router-dom";
-import "../css/reviews.scss";
+import "../css/comments.scss";
 
-
-const Comments = ({  }) => {
- 
-
+const Comments = ({}) => {
   return (
-    <div className="review-section w-[80%] mx-auto p-5 flex flex-col gap-4 ">
-      <div className="flex flex-col w-full px-5 gap-3 review-content">
+    <div className="comment-section">
+      <div className="comment-header">
         <h4 className="font-bold">Add Comment</h4>
-        <form className="w-full flex flex-col items-start justify-center gap-3" >
+        <form className="">
           <textarea
-            className="w-full h-24 border border-black rounded-md px-2 py-1"
             placeholder="Enter the comment..."
+            className="w-full rounded-md border border-black py-1 px-2"
           ></textarea>
-          <button
-            className="border px-3 py-2 border-black bg-black text-white font-semibold rounded-md text-sm"
-            type="submit"
-          >
-            Submit Comment
+          <button className="" type="submit">
+            Comment
           </button>
         </form>
       </div>
-      <div className="px-5 flex flex-col gap-3 reviews">
+      <div className="comments-container">
         <h4 className="font-bold text-md">Comments: </h4>
-          <div className="px-3">
-            <div
-              className="flex flex-col gap-2 px-2 py-1 m-0 "
-              style={{ borderBottom: "1px solid #FFFFFF" }}
-            >
-              <Link to="">
-                <h4 className="font-bold text-sm">Username</h4>
-              </Link>
-              <p className="m-0 text-sm">nice blog</p>
-              <div className="flex gap-3 py-2">
-                    <>
-                      <button className="border border-black bg-black text-white px-2 py-1 w-[90px] font-semibold text-sm rounded">
-                        edit
-                      </button>
-                  <>
-                    <button
-                      className="border border-black bg-black text-white px-2 py-1 w-[90px] font-semibold text-sm rounded"
-                    >
-                      delete
-                    </button>
-                  </>
-                    </>
-              </div>
+        <div className="comments">
+          <div className="pb-3" style={{ borderBottom: "1px solid #FFFFFF" }}>
+            <Link to="">
+              <h4 className="">Username</h4>
+            </Link>
+            <p className="">nice blog</p>
+            <div className="comment-btn">
+              <>
+                <button className="">edit</button>
+                <>
+                  <button className="">delete</button>
+                </>
+              </>
             </div>
           </div>
+        </div>
       </div>
     </div>
   );
