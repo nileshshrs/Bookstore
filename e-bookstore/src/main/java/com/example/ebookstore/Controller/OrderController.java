50c4075ced70670923d9e6aaa640db37a9a36848
nileshshrs <nileshshr54@gmail.com>
2024-01-09 19:44:58 +0545

    added api for fetching orders according to user and fetching all orders

--- e-bookstore/src/main/java/com/example/ebookstore/Controller/OrderController.java
+++ e-bookstore/src/main/java/com/example/ebookstore/Controller/OrderController.java
@@ -2,19 +2,21 @@ package com.example.ebookstore.Controller;
 
 import com.example.ebookstore.Service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v2/orders")
+@CrossOrigin(origins = {"http://localhost:5173","http://localhost:5174"})
 public class OrderController {
 
     private final OrderService orderService;
@@ -37,4 +39,22 @@ public class OrderController {
 
         return orderService.createOrder(cartItems, orderDate, shippingAddress, paymentMethod);
     }
+    @GetMapping("/all")
+    public List<Map<String, Object>> getAllOrders() {
+        return orderService.getAllOrders();
+    }
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<Map<String, Object>>> getOrdersByUserId(@PathVariable Long userId) {
+        try {
+            List<Map<String, Object>> userOrders = orderService.getOrdersByUserId(userId);
+            return ResponseEntity.ok(userOrders);
+        } catch (Exception e) {
+            // Handle the exception and return an appropriate response
+            Map<String, Object> errorResponse = new HashMap<>();
+            errorResponse.put("error", "Failed to fetch orders for user with ID: " + userId);
+            errorResponse.put("message", e.getMessage()); // Include the exception message
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Collections.singletonList(errorResponse));
+        }
+    }
+
 }
