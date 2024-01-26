0bfa2997ad126e433bfb8830c0829c8a8b998abc
bibhakta21 <bibhaktalamsal8@gmail.com>
2024-01-26 17:44:05 +0545

    User Detail Design Changed.

--- Frontend/e-bookstore/src/components/Userdetail.jsx
+++ Frontend/e-bookstore/src/components/Userdetail.jsx
@@ -16,6 +16,7 @@ const Userdetail = () => {
     roles: "admin", // Default role as "user"
   });
   const [validationErrors, setValidationErrors] = useState({});
+  const [activeTab, setActiveTab] = useState("UserDetails"); // Default active tab
 
   useEffect(() => {
     fetchUserDetails();
@@ -127,133 +128,170 @@ const Userdetail = () => {
       });
   };
 
+//delete user
+  const handleDeleteUser = async (userId) => {
+    try {
+      // Make an API call to delete the user
+      const response = await axios.delete(`http://localhost:8080/api/v2/users/delete/${userId}`);
+
+      // Handle successful deletion
+      toast.success(response.data);
+
+      // Refresh user table
+      fetchUserDetails();
+    } catch (error) {
+      // Handle deletion error
+      toast.error("Error deleting user");
+      console.error("Error deleting user:", error);
+    }
+  };
+
+  const toggleTab = () => {
+    setActiveTab(activeTab === "UserDetails" ? "CreateUser" : "UserDetails");
+  };
+
+  
   return (
     <div className="flex flex-col md:flex-row">
-    {/* User Details Section */}
-    <div className="w-full md:w-6/10 container border-t   mb-8" style={{ maxWidth: "80%" }}>
-      <h3 className="font-bold text-gray-900 p-2 mb-6 mt-8" style={{ fontSize: "30px", fontFamily: "Prata", fontWeight: "700" }}>
-        User Details
-      </h3>
+      {/* User Details Section */}
+      {activeTab === "UserDetails" && (
+        <div className="w-full md:w-6/10 container border-t mb-8" style={{ maxWidth: "80%" }}>
+          <h3 className="font-bold text-gray-900 p-2 mb-6 mt-8" style={{ fontSize: "30px", fontFamily: "Prata", fontWeight: "700" }}>
+            User Details
+          </h3>
 
-      <div className="max-w-lg rounded-md shadow-lg table-container" style={{ maxWidth: "100%" }}>
-        <table className="w-full  " style={{ fontFamily: "Prata", fontWeight: "100", fontSize: "15px" }}>
-          <thead>
-            <tr className="bg-gray-200">
-              <th className=" px-4 whitespace-nowrap">Username</th>
-              <th className=" px-4 whitespace-nowrap">Name</th>
-              <th className=" px-4 whitespace-nowrap">Email</th>
-              <th className=" px-4 whitespace-nowrap">Roles</th>
-              <th className=" px-4 whitespace-nowrap">Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {users.sort((a, b) => a.id - b.id).map((user) => (
-              <tr key={user.id}>
-                <td className="py-2 px-4 whitespace-nowrap" style={{ paddingBottom: "20px" }}>{user.username}</td>
-                <td className="py-2 px-4 whitespace-nowrap" style={{ paddingBottom: "20px" }}>{user.name}</td>
-                <td className="py-2 px-4 whitespace-nowrap" style={{ paddingBottom: "20px" }}>{user.email}</td>
-                <td className="py-2 px-4 whitespace-nowrap" style={{ paddingBottom: "20px" }}>{user.roles}</td>
-                <td className="py-2 px-4 whitespace-nowrap" style={{ paddingBottom: "20px" }}>
-                <button
-                      className="action-button rounded text-white bg-black p-1 w-18 mr-2"
-                      onClick={() => handleRoleChange(user.id, user.roles === 'admin' ? 'users' : 'admin')}
-                    >
-                      Change roles
-                    </button>
-                    <button
-                      className="action-button rounded text-white bg-red-500 p-1 w-18"
-                      
-                    >
-                      <AiOutlineDelete size={18} />
-                    </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </div>
-
-
-      {/* Form Section */}
-      <div className="w-full md:w-4/10 sm:p-8  mb-4 lg:w-2/5 xl:w-2/5" style={{ maxWidth: "100%" }}>
-        
-        <div className="rounded shadow  sm:p-6 mt-20" style={{maxHeight:"600px"}}>
-        <h3 className="font-bold text-gray-900  mb-6 " style={{ fontSize: "30px", fontFamily: "Prata", fontWeight: "700" }}>
-        Create User
-      </h3>
-          <label htmlFor="username" className="font-semibold text-gray-700 block pb-1">
-            Username
-          </label>
-          <input
-            name="username"
-            className={`border-2 rounded-r px-4 py-2 w-full ${validationErrors.username ? "border-red-500" : ""}`}
-            type="text"
-            value={formData.username}
-            onChange={handleInputChange}
-            autoComplete="off"
-          />
-          {validationErrors.username && <div className="text-red-500">{validationErrors.username}</div>}
-
-          <label htmlFor="name" className="font-semibold text-gray-700 block pb-1">
-            Name
-          </label>
-          <input
-            name="name"
-            className={`border-2 rounded-r px-4 py-2 w-full ${validationErrors.name ? "border-red-500" : ""}`}
-            type="text"
-            value={formData.name}
-            onChange={handleInputChange}
-            autoComplete="off"
-          />
-          {validationErrors.name && <div className="text-red-500">{validationErrors.name}</div>}
-
-          <label htmlFor="email" className="font-semibold text-gray-700 block pb-1">
-            Email
-          </label>
-          <input
-            name="email"
-            className={`border-2 rounded-r px-4 py-2 w-full ${validationErrors.email ? "border-red-500" : ""}`}
-            type="text"
-            value={formData.email}
-            onChange={handleInputChange}
-          />
-          {validationErrors.email && <div className="text-red-500">{validationErrors.email}</div>}
-
-          <label htmlFor="password" className="font-semibold text-gray-700 block pb-1">
-            Password
-          </label>
-          <input
-            name="password"
-            className={`border-2 rounded-r px-4 py-2 w-full ${validationErrors.password ? "border-red-500" : ""}`}
-            type="password"
-            value={formData.password}
-            onChange={handleInputChange}
-          />
-          {validationErrors.password && <div className="text-red-500">{validationErrors.password}</div>}
-
-          {/* Roles Input */}
-          <label htmlFor="roles" className="font-semibold text-gray-700 block pb-1">
-            Roles
-          </label>
-          <input
-            type="text"
-            id="roles"
-            name="roles"
-            className="border-2 rounded-r px-4 py-2 w-full"
-            value={formData.roles}
-            onChange={handleInputChange}
-          />
-
-          {/* Form Submission */}
-          <button
-            onClick={handleFormSubmit}
-            className="border ml-3 px-3 py-2 mt-4 border-black bg-black text-white font-semibold rounded-md text-sm"
-          >
-            Create
-          </button>
+          <div className="max-w-lg rounded-md shadow-lg table-container" style={{ maxWidth: "100%" }}>
+            <table className="w-full " style={{ fontFamily: "Prata", fontWeight: "100", fontSize: "15px" }}>
+              <thead>
+                <tr className="bg-gray-200">
+                  <th className="px-4 whitespace-nowrap">Username</th>
+                  <th className="px-4 whitespace-nowrap">Name</th>
+                  <th className="px-4 whitespace-nowrap">Email</th>
+                  <th className="px-4 whitespace-nowrap">Roles</th>
+                  <th className="px-4 whitespace-nowrap">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {users.sort((a, b) => a.id - b.id).map((user) => (
+                  <tr key={user.id}>
+                    <td className="py-2 px-4 whitespace-nowrap" style={{ paddingBottom: "20px" }}>{user.username}</td>
+                    <td className="py-2 px-4 whitespace-nowrap" style={{ paddingBottom: "20px" }}>{user.name}</td>
+                    <td className="py-2 px-4 whitespace-nowrap" style={{ paddingBottom: "20px" }}>{user.email}</td>
+                    <td className="py-2 px-4 whitespace-nowrap" style={{ paddingBottom: "20px" }}>{user.roles}</td>
+                    <td className="py-2 px-4 whitespace-nowrap" style={{ paddingBottom: "20px" }}>
+                      <button
+                        className="action-button rounded text-white bg-black p-1 w-18 mr-2"
+                        onClick={() => handleRoleChange(user.id, user.roles === 'admin' ? 'users' : 'admin')}
+                      >
+                        Change roles
+                      </button>
+                      <button
+                        className="action-button rounded text-white bg-black p-1 w-18"
+                        onClick={() => handleDeleteUser(user.id)}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
+      )}
+
+      {/* Create User Section */}
+      {activeTab === "CreateUser" && (
+        <div className="w-full md:w-4/10 sm:p-8 mb-4 lg:w-2/5 xl:w-2/5" style={{ maxWidth: "100%",marginLeft:"360px" }}>
+          <div className="rounded shadow sm:p-6 mt-20" style={{ maxHeight: "700px" }}>
+            <h3 className="font-bold text-gray-900 mb-6 " style={{ fontSize: "30px", fontFamily: "Prata", fontWeight: "700" }}>
+              Create User
+            </h3>
+            <label htmlFor="username" className="font-semibold text-gray-700 block pb-1">
+              Username
+            </label>
+            <input
+              name="username"
+              className={`border-2 rounded-r px-4 py-2 w-full ${validationErrors.username ? "border-red-500" : ""}`}
+              type="text"
+              value={formData.username}
+              onChange={handleInputChange}
+              autoComplete="off"
+            />
+            {validationErrors.username && <div className="text-red-500">{validationErrors.username}</div>}
+
+            <label htmlFor="name" className="font-semibold text-gray-700 block pb-1">
+              Name
+            </label>
+            <input
+              name="name"
+              className={`border-2 rounded-r px-4 py-2 w-full ${validationErrors.name ? "border-red-500" : ""}`}
+              type="text"
+              value={formData.name}
+              onChange={handleInputChange}
+              autoComplete="off"
+            />
+            {validationErrors.name && <div className="text-red-500">{validationErrors.name}</div>}
+
+            <label htmlFor="email" className="font-semibold text-gray-700 block pb-1">
+              Email
+            </label>
+            <input
+              name="email"
+              className={`border-2 rounded-r px-4 py-2 w-full ${validationErrors.email ? "border-red-500" : ""}`}
+              type="text"
+              value={formData.email}
+              onChange={handleInputChange}
+            />
+            {validationErrors.email && <div className="text-red-500">{validationErrors.email}</div>}
+
+            <label htmlFor="password" className="font-semibold text-gray-700 block pb-1">
+              Password
+            </label>
+            <input
+              name="password"
+              className={`border-2 rounded-r px-4 py-2 w-full ${validationErrors.password ? "border-red-500" : ""}`}
+              type="password"
+              value={formData.password}
+              onChange={handleInputChange}
+            />
+            {validationErrors.password && <div className="text-red-500">{validationErrors.password}</div>}
+
+            {/* Roles Input */}
+            <label htmlFor="roles" className="font-semibold text-gray-700 block pb-1">
+              Roles
+            </label>
+            <input
+              type="text"
+              id="roles"
+              name="roles"
+              className="border-2 rounded-r px-4 py-2 w-full"
+              value={formData.roles}
+              onChange={handleInputChange}
+              disabled
+            />
+
+            {/* Form Submission */}
+            <button
+              onClick={handleFormSubmit}
+              className="border ml-3 px-3 py-2 mt-4 border-black bg-black text-white font-semibold rounded-md text-sm"
+            >
+              Create
+            </button>
+          </div>
+        </div>
+      )}
+
+      {/* Toggle Tab Button */}
+      <div className="absolute  right-0 p-4 ">
+        <button
+          className="border px-3 py-2 border-black bg-black text-white font-semibold rounded-md text-sm"
+          onClick={toggleTab}
+        >
+          {activeTab === "UserDetails" ? "Create User" : "User Details"}
+        </button>
       </div>
+
       {/* Toastify Notifications */}
       <ToastContainer position="bottom-right" autoClose={3000} />
     </div>
